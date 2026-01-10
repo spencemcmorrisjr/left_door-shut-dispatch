ce70a7c332bb7064258b3e3210374b53957d3207
spencemcmorrisjr <spencemcmorrisjr@gmail.com>
2026-01-10 14:44:42 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 document.getElementById("loginBtn").onclick = function () {
-  alert("Login system coming next!");
+  let email = prompt("Enter admin email:");
+  let password = prompt("Enter password:");
+
+  if(email === "LeftDoorShutDispatch@gmail.com" && password === "Setup123!") {
+    window.location.href = "dashboard.html";
+  } else {
+    alert("Invalid login (test mode)");
+  }
 };
