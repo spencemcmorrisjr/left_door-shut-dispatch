54b65c368afe774863c5dd400926a761e720404f
spencemcmorrisjr <spencemcmorrisjr@gmail.com>
2026-01-11 11:20:27 -0600

    Update script.js

--- script.js
+++ script.js
@@ -20,3 +20,39 @@ function saveTruckProfile() {
   document.getElementById("truckProfileStatus").innerText =
     "Truck profile saved successfully.";
 }
+function loadFuelStops() {
+  const fuelStops = [
+    {
+      brand: "Love's",
+      store: "#123",
+      exit: "Exit 46 – I-30",
+      city: "Prescott, AR",
+      address: "100 Loves Way",
+      map: "https://www.google.com/maps/search/?api=1&query=100+Loves+Way+Prescott+AR"
+    },
+    {
+      brand: "Pilot",
+      store: "#452",
+      exit: "Exit 44 – I-30",
+      city: "Prescott, AR",
+      address: "250 Pilot Dr",
+      map: "https://www.google.com/maps/search/?api=1&query=250+Pilot+Dr+Prescott+AR"
+    }
+  ];
+
+  const list = document.getElementById("fuelStopsList");
+  list.innerHTML = "";
+
+  fuelStops.forEach(stop => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+      <strong>${stop.brand} ${stop.store}</strong><br>
+      ${stop.exit}<br>
+      ${stop.city}<br>
+      ${stop.address}<br>
+      <a href="${stop.map}" target="_blank">Open in Maps</a>
+    `;
+    list.appendChild(li);
+  });
+}
+loadFuelStops();
